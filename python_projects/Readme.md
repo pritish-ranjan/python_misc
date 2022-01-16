9934c592cd3b9a30e6e25b11b9c932bb4a9055d3
Pritish Ranjan <pritish.ranjan1@gmail.com>
2022-01-16 12:07:01 +0000

    misc/updated-name

--- python_projects/Readme.md
+++
@@ -1,1 +0,0 @@
-# This project has miscellaneous problems with solutions 
--- python_projects/python_standalone/requests_test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://api.randomuser.me/"
-
-response = requests.get(url)
-
-print(response.json())
